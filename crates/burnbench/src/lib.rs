afffdfad57cdd253a5a4b3448e3c6174c7840dfd
syl20bnr <sylvain.benner@tracel.ai>
2025-07-23 01:06:46 -0400

    fix: url to Burn website in dev mode

--- crates/burnbench/src/lib.rs
+++ crates/burnbench/src/lib.rs
@@ -19,7 +19,8 @@ const USER_BENCHMARK_SERVER_URL: &str = if cfg!(debug_assertions) {
     "https://user-benchmark-server-812794505978.northamerica-northeast1.run.app/v1/"
 };
 
-#[cfg(test)]
-const USER_BENCHMARK_WEBSITE_URL: &str = "http://localhost:4321/";
-#[cfg(not(test))]
-const USER_BENCHMARK_WEBSITE_URL: &str = "https://burn.dev/";
+const USER_BENCHMARK_WEBSITE_URL: &str = if cfg!(debug_assertions) || cfg!(test) {
+    "http://localhost:4321/"
+} else {
+    "https://burn.devel/"
+};
