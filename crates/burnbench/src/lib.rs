9bba53414ac69d7f8fc1dc82e52b1ae14256b32a
syl20bnr <sylvain.benner@tracel.ai>
2025-07-29 13:46:01 -0400

    Fix website URL

--- crates/burnbench/src/lib.rs
+++ crates/burnbench/src/lib.rs
@@ -22,7 +22,7 @@ const USER_BENCHMARK_SERVER_URL: &str = if cfg!(debug_assertions) {
 const USER_BENCHMARK_WEBSITE_URL: &str = if cfg!(debug_assertions) || cfg!(test) {
     "http://localhost:4321/"
 } else {
-    "https://burn.devel/"
+    "https://burn.dev/"
 };
 
 #[macro_export]
