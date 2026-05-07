60b7142e946f693de519adb2f630972b3afb84de
Charles23R <renaudcharles14@hotmail.com>
2026-05-07 16:03:16 -0400

    add to_data and prints

--- crates/backend-comparison/benches/all_reduce_old.rs
+++ crates/backend-comparison/benches/all_reduce_old.rs
@@ -62,6 +62,9 @@ mod distributed_benchmarks {
 
             for recv in recvs {
                 let tensor = recv.recv().unwrap();
+                let data = tensor.to_data();
+                println!("tensor: {tensor}");
+                println!("data: {data}");
                 out.push(tensor);
             }
             out
