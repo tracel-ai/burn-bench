545a8e8fadf035e75ce04697a28f6c551a7f1e33
Thierry Cantin-Demers <thierrycantindemers@gmail.com>
2026-06-12 15:39:45 -0400

    quantized reduce benchmarkss

--- crates/backend-comparison/benches/reduce.rs
+++ crates/backend-comparison/benches/reduce.rs
@@ -1,6 +1,15 @@
 use burn::tensor::Int;
+use burn::tensor::quantization::{
+    BlockSize, QuantLevel, QuantMode, QuantParam, QuantScheme, QuantStore, QuantValue,
+};
 use burn::tensor::{Device, Distribution, Shape, Tensor};
 use burnbench::{Benchmark, BenchmarkResult, run_benchmark};
+
+#[derive(Clone, Copy, Debug)]
+enum Quantization {
+    None,
+    Quantized(QuantScheme),
+}
 
 enum Instruction {
     ArgMin(usize),
@@ -15,17 +24,23 @@ struct ReduceBenchmark {
     shape: Shape,
     device: Device,
     tensor: Tensor<3>,
+    quantized: bool,
 }
 
 impl ReduceBenchmark {
-    pub fn new(instruction: Instruction, device: Device) -> Self {
+    pub fn new(instruction: Instruction, device: Device, quantization: Quantization) -> Self {
         let shape = Shape::new([32, 512, 4096]);
         let tensor = Tensor::random(shape.clone(), Distribution::Default, &device);
+        let tensor = match quantization {
+            Quantization::None => tensor,
+            Quantization::Quantized(quant_scheme) => tensor.quantize_dynamic(&quant_scheme),
+        };
         Self {
             instruction,
             shape,
             device,
             tensor,
+            quantized: matches!(quantization, Quantization::Quantized(_)),
         }
     }
 }
@@ -34,6 +49,13 @@ pub enum ReduceOutput<const D: usize> {
     Arg(Tensor<D, Int>),
     Dim(Tensor<D>),
     Full(Tensor<1>),
+}
+
+fn quantized_name(quantized: bool) -> String {
+    match quantized {
+        true => "-quantized".to_string(),
+        false => "".to_string(),
+    }
 }
 
 impl Benchmark for ReduceBenchmark {
@@ -65,30 +87,42 @@ impl Benchmark for ReduceBenchmark {
     }
 
     fn name(&self) -> String {
+        let quantized = quantized_name(self.quantized);
         match self.instruction {
             Instruction::ArgMin(axis) => {
                 format!(
-                    "reduce-argmin-{axis}-{:?}",
-                    self.device.settings().float_dtype
+                    "reduce-argmin-{axis}-{:?}{}",
+                    self.device.settings().float_dtype,
+                    quantized
                 )
             }
             Instruction::ArgMinFused(axis) => {
                 format!(
-                    "reduce-argmin-{axis}-fused-{:?}",
-                    self.device.settings().float_dtype
+                    "reduce-argmin-{axis}-fused-{:?}{}",
+                    self.device.settings().float_dtype,
+                    quantized
                 )
             }
             Instruction::SumDim(axis) => {
-                format!("reduce-sum-{axis}-{:?}", self.device.settings().float_dtype)
+                format!(
+                    "reduce-sum-{axis}-{:?}{}",
+                    self.device.settings().float_dtype,
+                    quantized
+                )
             }
             Instruction::SumDimFused(axis) => {
                 format!(
-                    "reduce-sum-{axis}-fused-{:?}",
-                    self.device.settings().float_dtype
+                    "reduce-sum-{axis}-fused-{:?}{}",
+                    self.device.settings().float_dtype,
+                    quantized
                 )
             }
             Instruction::Sum => {
-                format!("reduce-sum-full-{:?}", self.device.settings().float_dtype)
+                format!(
+                    "reduce-sum-full-{:?}{}",
+                    self.device.settings().float_dtype,
+                    quantized
+                )
             }
         }
         .to_lowercase()
@@ -107,26 +141,52 @@ impl Benchmark for ReduceBenchmark {
 fn bench(device: &Device) -> Vec<BenchmarkResult> {
     let mut benchmarks = Vec::new();
 
-    for axis in 0..3 {
-        benchmarks.push(ReduceBenchmark::new(
-            Instruction::ArgMin(axis),
-            device.clone(),
-        ));
-        benchmarks.push(ReduceBenchmark::new(
-            Instruction::ArgMinFused(axis),
-            device.clone(),
-        ));
-        benchmarks.push(ReduceBenchmark::new(
-            Instruction::SumDim(axis),
-            device.clone(),
-        ));
-        benchmarks.push(ReduceBenchmark::new(
-            Instruction::SumDimFused(axis),
-            device.clone(),
-        ));
+    let base_quant_scheme = QuantScheme {
+        value: QuantValue::Q8F,
+        param: QuantParam::F32,
+        store: QuantStore::PackedU32(0),
+        level: QuantLevel::Tensor,
+        mode: QuantMode::Symmetric,
+    };
+
+    let quant_schemes = [QuantScheme {
+        value: QuantValue::Q8S,
+        level: QuantLevel::Block(BlockSize::new([32])),
+        ..base_quant_scheme
+    }]
+    .into_iter()
+    .map(|quant_scheme| Quantization::Quantized(quant_scheme));
+
+    for quantization in [Quantization::None].into_iter().chain(quant_schemes) {
+        for axis in 0..3 {
+            benchmarks.push(ReduceBenchmark::new(
+                Instruction::ArgMin(axis),
+                device.clone(),
+                quantization,
+            ));
+            benchmarks.push(ReduceBenchmark::new(
+                Instruction::ArgMinFused(axis),
+                device.clone(),
+                quantization,
+            ));
+            benchmarks.push(ReduceBenchmark::new(
+                Instruction::SumDim(axis),
+                device.clone(),
+                quantization,
+            ));
+            benchmarks.push(ReduceBenchmark::new(
+                Instruction::SumDimFused(axis),
+                device.clone(),
+                quantization,
+            ));
+        }
     }
 
-    benchmarks.push(ReduceBenchmark::new(Instruction::Sum, device.clone()));
+    benchmarks.push(ReduceBenchmark::new(
+        Instruction::Sum,
+        device.clone(),
+        Quantization::None,
+    ));
     benchmarks.into_iter().map(run_benchmark).collect()
 }
 
